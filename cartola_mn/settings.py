b4f508b99b10167a2d6fea90a8a1b0a2f5ece239
Ikaro Pinheiro Lopes <ikaropinheiro6@gmail.com>
2019-05-24 01:04:17 -0300

    Config static files

--- cartola_mn/settings.py
+++ cartola_mn/settings.py
@@ -7,7 +7,7 @@ from prettyconf import config
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
 STATIC_DIR = os.path.join(BASE_DIR, "static")
-
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -109,8 +109,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIR = [
-    STATIC_DIR,
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
 ]
 
 django_heroku.settings(locals())
